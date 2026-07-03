1e5e008c25e8cbbb258d59e73d9be75bad6ac60b
fengy2001 <fengyuanuc@gmail.com>
2026-07-03 00:12:33 +0000

    basic lifespan and create-project endpoint

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from overleafce-fastapi!")
+from fastapi import FastAPI, UploadFile, HTTPException, Form
+from contextlib import asynccontextmanager
+import asyncio
 
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    yield
+    
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/create-project")
+async def create_project(
+        owner_email: str = Form(...),
+        project_name: str = Form(...),
+        zip_file: UploadFile = ...,
+    ):
+
+    zip_bytes = await zip_file.read()
+    proc = await asyncio.create_subprocess_exec(
+        "docker", "exec", "-i", "sharelatex",
+        "node", "modules/custom-scripts/create-project.mjs",
+        owner_email, project_name,
+        stdin=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await proc.communicate(input=zip_bytes)
+    if proc.returncode != 0:
+        raise HTTPException(500, stderr.decode())
+
+    return {"result": stdout.decode().strip()}

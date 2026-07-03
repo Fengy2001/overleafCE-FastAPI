39846102513556fcad6749bb10acbd44c8f76758
fengy2001 <fengyuanuc@gmail.com>
2026-07-03 00:25:14 +0000

    modularize function execution parameters

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI, UploadFile, HTTPException, Form
 from contextlib import asynccontextmanager
 import asyncio
+import json
 
 @asynccontextmanager
 async def lifespan(app : FastAPI):
+    with open("./secrets/secret.json") as file:
+        data = json.load(file)
+    app.state.container_name = data["container_name"]
+    app.state.mjs_script = data["mjs-script"]
     yield
     
 
@@ -18,8 +23,8 @@ async def create_project(
 
     zip_bytes = await zip_file.read()
     proc = await asyncio.create_subprocess_exec(
-        "docker", "exec", "-i", "sharelatex",
-        "node", "modules/custom-scripts/create-project.mjs",
+        "docker", "exec", "-i", app.state.container_name,
+        "node", app.state.mjs_script,
         owner_email, project_name,
         stdin=asyncio.subprocess.PIPE,
         stdout=asyncio.subprocess.PIPE,
